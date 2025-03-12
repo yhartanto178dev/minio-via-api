8d334519440750cc8b311c551c32ace1229401cf
yhartanto178dev <badansdm34@gmail.com>
2025-03-12 14:45:31 +0700

    Implement bucket management functions: check existence, remove, and set tagging

--- bucket.go
+++ bucket.go
@@ -6,8 +6,11 @@ import (
 	"log"
 
 	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/tags"
 )
 
+// /Bucket Section
+// /* This section is Bucket Section */
 func MakeBucket(NameBucket, minioUrl, accessKeyID, secreetAccessKey string) (string, error) {
 	// Make a new bucket called my-bucketname.
 	minioClient := NewMinioAPi(minioUrl, accessKeyID, secreetAccessKey)
@@ -30,6 +33,49 @@ func ListBuckets(minioUrl, accessKeyID, secreetAccessKey string) ([]minio.Bucket
 	return buckets, nil
 }
 
+func BucketsExists(NameBucket, minioUrl, accessKeyID, secreetAccessKey string) (bool, error) {
+	// Check if a bucket exists.
+	minioClient := NewMinioAPi(minioUrl, accessKeyID, secreetAccessKey)
+	found, err := minioClient.BucketExists(context.Background(), NameBucket)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if found {
+		fmt.Println("Bucket found")
+	} else {
+		fmt.Println("Bucket not found")
+	}
+	return found, nil
+}
+
+func RemoveBuckets(NameBucket, minioUrl, accessKeyID, secreetAccessKey string) (string, error) {
+	// Remove a bucket called my-bucketname.
+	minioClient := NewMinioAPi(minioUrl, accessKeyID, secreetAccessKey)
+	err := minioClient.RemoveBucket(context.Background(), NameBucket)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Successfully removed", NameBucket)
+	return "Success", nil
+}
+
+func SetBucketTagging(NameBucket, minioUrl, accessKeyID, secreetAccessKey string, NewTags map[string]string) (string, error) {
+	tags, errTag := tags.NewTags(NewTags, false)
+	if errTag != nil {
+		log.Fatalln(errTag)
+	}
+	// Set bucket tagging
+	minioClient := NewMinioAPi(minioUrl, accessKeyID, secreetAccessKey)
+	err := minioClient.SetBucketTagging(context.Background(), NameBucket, tags)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Successfully set bucket tagging on", NameBucket)
+	return "Success", nil
+}
+
+///Object Section
+/* This section is Object Section */
 func FPutObjects(bucketName, objectName, filePath, minioUrl, accessKeyID, secreetAccessKey string) (string, error) {
 	// Upload the zip file
 	minioClient := NewMinioAPi(minioUrl, accessKeyID, secreetAccessKey)
